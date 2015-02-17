106eb486e0f90ffe5ff02c60a5df332c96fd5434
Jim Moffitt <jimmoffitt@yahoo.com>
2015-02-16 23:08:28 -0700

    Implemented 'max number' and 'min threshold' limits on negations.

--- pt_negationator.rb
+++ pt_negationator.rb
@@ -1,31 +1,50 @@
-require_relative './pt_rules'
-require_relative './pt_restful'
+require_relative './common/pt_rules'
+require_relative './common/pt_http'
+require_relative './common/pt_search_helper'
 
 require 'json'
+require 'yaml'
 require 'logging'
 require 'base64'
+
+#Uses Search API to assess negation effectiveness.
 
 class PTNegationator
 
   attr_accessor :account_name, :product, :publisher, :label,
                 :http, :urlSearch,  #need a HTTP object to make requests of
                 :search_api_creds,
-                :rule_set,
-                :base_rule,
-                :negations,
                 :count_interval,
-                :request_sleep
+                :request_sleep,
 
+                :rule_set, # <-- PtRules.new
+                :base_rule,
+                
+                :negations,
+                :saved_results,
+                :min_threshold,
+                :max_number,
+                
+                :project_name
+  
   def initialize
+
+    # @project_name -- no default.
+    @saved_results = './SavedCounts/'
+    @min_threshold = 30
+    @max_number = 50
+    
     @negations = []
+
     @search_api_creds = {}
+    @request_sleep = 5 #seconds
 
     @product = 'track' #Only product supported, only one needed.
     @publisher = 'twitter'
     @count_interval = 'day'
     
-    @http = PtRESTful.new
-    @rule_set = PTRules.new
+    @http = PtHTTP.new
+    @pt_rules = PTRules.new
   end
 
   def build_rules 
@@ -37,17 +56,16 @@ class PTNegationator
     rules << rule
     
     @negations.each do |negation|
-      rule = PT_Rule_Negation.new
+      rule = PT_Negation_Test_Rule.new
       rule.value = "-#{negation.strip} (#{@base_rule})"
       rule.negation = negation.strip
       rules << rule
     end
     
-    @rule_set = rules
+    @pt_rules = rules
     
   end
   
-
   #Load in the configuration file details, setting many object attributes.
   def get_app_config(config_file)
 
@@ -88,9 +106,14 @@ class PTNegationator
     rescue
       @outbox = "./output"
     end
-
     @request_sleep = config['app']['request_sleep']
 
+    #Negationator settings!
+    @project_name = config['negations']['project_name']
+    @saved_results = config['negations']['saved_results']
+    @min_threshold = config['negations']['min_threshold']
+    @max_number = config['negations']['max_number']
+    
   end
 
   def get_date_string(time)
@@ -150,51 +173,63 @@ class PTNegationator
     end
   end
   
-  def check(return_rule_set=nil)
+  def get_top_negations
+
+  end
+  
+  def check_negations(return_rule_set=nil)
     
     return_rules = false
     
     if !return_rule_set.nil? then
       return_rules = true
     end
-
-    build_rules
     
-    #Establish static 30-day start and end times: base rule results can otherwise change during the negation checks.
-    #High volume base rules are likely to change during the multiple negation rule checks... 
-    end_time = Time.now.utc 
-    start_time = end_time - (30 * 24 * 60 * 60) # 30 days.
+    
+    #TODO:
+    #Check for saved counts... Do not want to slam Search API when we don't need to
+    #if /SavedCounts/oApp.project_name.json exists load it!
 
+    results_file = "#{@saved_results}/#{@project_name}.yaml"
 
-    #loop through negations, adding one negation at a time to base rule, than get 30-day counts
-    @rule_set.each do |rule|
+    #If file exists, then load it
+    if File.exist? results_file then 
+      serialized_object = File.open(results_file, "rb")
+      @pt_rules = YAML::load(serialized_object)
+    
+    else #Go get counts, and populate @pt_rules objects.
       
-      puts rule.value
-      counts_response = get_search_counts(@search_api_creds, rule.value, start_time, end_time)
-      rule.count_30_day = get_count_total(counts_response)
-      puts rule.count_30_day
+      build_rules
+
+      #Establish static 30-day start and end times: base rule results can otherwise change during the negation checks.
+      #High volume base rules are likely to change during the multiple negation rule checks...
+      end_time = Time.now.utc
+      start_time = end_time - (30 * 24 * 60 * 60) # 30 days.
+
+      #loop through negations, adding one negation at a time to base rule, than get 30-day counts
+      @pt_rules.each do |rule|
+        
+        puts rule.value
+        counts_response = get_search_counts(@search_api_creds, rule.value, start_time, end_time)
+        rule.count_30_day = get_count_total(counts_response)
+        puts rule.count_30_day
+        
+      end
+  
+      #Saved current results
+      serialized_objects = YAML::dump(@pt_rules)
+      File.open(results_file, 'w') {|f| f.write(serialized_objects) }
+    end
       
-    end
-
-    #Sanity-check, same results for base rules.
-    counts_response = get_search_counts(@search_api_creds, @rule_set[0].value, start_time, end_time)
-    
-    if counts_response.include? 'error' then
-      puts @rule_set[0].value
-      sleep 1
-    end
-    
-    puts get_count_total(counts_response)
-
     #Sort results and write output
     
-    baseline = @rule_set[0].count_30_day
+    baseline = @pt_rules[0].count_30_day
 
     results = []
     results_sorted = []
     
-    @rule_set.each do |rule|
-      if rule.is_a? PT_Rule_Negation then
+    @pt_rules.each do |rule|
+      if rule.is_a? PT_Negation_Test_Rule then
         difference = rule.count_30_day - baseline
         rule.effect = difference
         puts "Change in counts: #{difference}"
@@ -208,13 +243,41 @@ class PTNegationator
     results_sorted.each do |result|
       puts "#{result.negation}: #{result.effect} " unless result.effect > 0 #TODO: bug alert!
     end
+    
+    negations_before = @pt_rules.length
+    
+    #Eliminate negations under the effectiveness threshold.
+    negations = []
+    @pt_rules.each do |rule|
+      if rule.is_a? PT_Negation_Test_Rule then 
+        if rule.effect <= (-1 * @min_threshold) then
+          negations << rule
+        end
+      end
+    end
+
+    @pt_rules = negations
+    negations_after = @pt_rules.length
+    puts "Eliminated #{negations_before - negations_after} rules due to minimum threshold of #{@min_threshold}"
+
+    negations_before = @pt_rules.length
+    negations = []
+
+    #Eliminate negations over the max number to keep.
+    if @pt_rules.length > @max_number
+      (@pt_rules.length - @max_number).times do |item|
+        @pt_rules.pop
+      end
+    end
+
+    @pt_rules = negations
+    negations_after = @pt_rules.length
+    puts "Eliminated #{negations_before - negations_after} rules due to maximum number, #{@max_number}, to keep"
 
     if return_rules == true then
-      return @rule_set
+      return @pt_rules
     end
-    
   end
-
 end
 
 
@@ -231,11 +294,16 @@ if __FILE__ == $0  #This script code is executed when running this file.
 
   oApp.get_app_config(config_file) #This triggers loading of streams.
 
-  negations = "\"@love_absolut\" OR \"@_absolut_truth\" OR \"@absolut_blank\" OR \"@absolutamber\" OR \"@d_absolut_truth\" OR \"@elyx__\" OR \"@socallme_elyx\" OR \"elyxyak\" OR \"erotikgirls\" OR \"formel 1\" OR\"formula 1\" OR \"dj smirnoff\" OR \"dj absolut\" OR \"dj_smirnoff\" OR \"dj_smirnoff_ice\" OR \"karin smirnoff\" OR \"karina smirnoff\" OR \"katrina smirnoff\" OR \"kyza smirnoff\" OR \"oleg smirnoff\" OR \"pere smirnoff\" OR \"quick get some smirnoff ice\" OR \"red bull media\" OR \"serg smirnoff\" OR \"serg_smirnoff\" OR \"smirnoff centre\" OR \"smirnoff hotel\" OR \"smirnoff dj\" OR \"smirnoff music centre\" OR\"smirnoff turntable\" OR \"smirnoff type\" OR \"smirnoff wrote\" OR \"viagra\" OR \"victoria smirnoff\" OR \"yaakov smirnoff\" OR \"yakov smirnoff\" OR \"zmey smirnoff\" OR \"absolut nicht\" OR \"absolut nichts\"OR \"absolut repair\" OR \"absolut_blank\" OR \"absolut_truth\" OR \"absolut_watkins\" OR \"absolute_pepper\" OR \"dancing with the\" OR \"dancing with the stars\" OR \"natalia smirnoff\" OR \"chilling in the sea\" OR \"d_absolut_truth\" OR \"erotik girls\" OR \"garota_smirnoff\" OR \"nick smirnoff\" OR \"dimitri smirnoff\" OR \"diully kethellyn\" OR \"karina-smirnoff\" OR \"l'oreal\" OR \"alexander smirnoff\" OR\"minichill\" OR \"anna smirnoff\" OR \"doctor smirnoff\" OR \"penis\" OR \"socallme_elyx\" OR \"nvidea\" OR \"von smirnoff\" OR \"absolut inte\" OR \"absolut värsta\" OR \"#rasism\" OR \"doodle\" OR \"le petit\"OR \"yak\" OR \"little things i like\" OR \"mouvmatin\" OR \"arena naţională\" OR \"iphone 6\" OR \"windos\" OR \"mozilla\" OR \"http://forum.softpedia.com/\" OR \"scf\" OR sex OR \"sex video\" OR toys OR \"absolut garden\""
+
+  oApp.project_name = "dev" #Negation have 'projects' because negation analysis may require large amounts of Search API calls.
+  #Before making calls, it checks to see if a saved 'project' set of negation results exists.
+
+  #oApp.negations is an array of independent 'negation' clauses.
+  negations = "ice OR icy OR \"heavy rain\" OR scared OR roads OR  \"don't like\" OR \"flip-flops\" OR \"heat lamps\" OR \"sand trap\" OR \"mud season\" OR \"can't stand\" OR \"heat wave\" OR \"fast melt\""
   oApp.negations = negations.split("OR")
 
-  oApp.base_rule = "absolut OR #absolut OR #absolutamber OR #absolutapeach OR #absolutberriacai OR #absolutchicago OR #absolutciron OR #absolutelyx OR #absolutgreyhound OR #absolutinspire OR #absolutkarnival"
+  oApp.base_rule = "(snow OR powder OR mountain OR \"snow storm\" OR snowing OR snowfall OR \"ski resort\" OR breckenridge OR vail OR keystone OR aspen) profile_region:colorado"
 
-  oApp.check
+  oApp.check_negations
 
 end
